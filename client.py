42cc68b7014b796c7e9e8f90380aefd7780b426e
kaoskekw <shumkovalex@yandex.ru>
2022-12-22 17:26:54 +0700

    socket_final

--- client.py
+++ client.py
@@ -1,36 +1,56 @@
 import socket
-import threading
-import sys
-
-HOST = '127.0.0.1'
-PORT = 9090
+from threading import Thread
 
 
-def listen(server):
+def listen(server, client):
     while True:
-        msg = server.recv(1024).decode()
-        print(msg)
+        message = server.recv(1024).decode()
+        print('\n' + message + '\n' + client + ':',end='')
 
 
 def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.connect((HOST, PORT))
+    server = socket.socket()
+    server.connect(('127.0.0.1', 9111))
 
-    username = input("Enter your nickname: ")
-    server.send(username.encode('utf-8'))
-    print(server.recv(1024).decode())
+    print("""
+             *     ,MMM8&&&.            *
+                  MMMM88&&&&&    .
+                 MMMM88&&&&&&&
+     *           MMM88&&&&&&&&
+                 MMM88&&&&&&&&
+                 'MMM88&&&&&&'
+                   'MMM8&&&'      *
+        |\___/|
+        )     (             .              '
+       =\     /=
+         )===(       *
+        /     \\
+        |     |
+       /       \\
+       \       /
+_/\_/\_/\__  _/_/\_/\_/\_/\_/\_/\_/\_/\_/\_
+|  |  |  |( (  |  |  |  |  |  |  |  |  |  |
+|  |  |  | ) ) |  |  |  |  |  |  |  |  |  |
+|  |  |  |(_(  |  |  |  |  |  |  |  |  |  |
+|  |  |  |  |  |  |  |  |  |  |  |  |  |  |
+KEK_CHAT |  |  |  |  |  |  |  |  |  |  |  |
+""")
 
-    thread = threading.Thread(target=listen, args=(server,))
+    client = input("Enter your nickname: ")
+
+    server.send((client.encode()))
+
+    thread = Thread(target=listen, args=(server, client,))
     thread.daemon = True
     thread.start()
 
     while True:
-        msg = input()
+        message = input(client + ' : ')
 
-        if msg == "/disconnect":
+        if message == '/exit':
             break
 
-        server.send(('> ' + username + ': ' + msg).encode('utf-8'))
+        server.send((client + ' : ' + message).encode())
 
     server.close()
 
--- server.py
+++ server.py
@@ -1,42 +1,45 @@
 import socket
-import threading
-
-HOST = '127.0.0.1'
-PORT = 9090
-CONNECTIONS = 5
+from threading import Thread
 
 
-def broadcast(msg, client_list):
-    for client in client_list:
-        client.send(msg.encode('utf-8'))
-    print(msg)
+def up_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('127.0.0.1', 9111))
+    server.listen(20)
+    return server
 
 
-def listen(msg, client_list):
+def listen(conn, client_list):
     while True:
         try:
-            msg = msg.recv(1024).decode
+            message = conn.recv(1024).decode()
         except:
-            client_list.remove(msg)
+            client_list.remove(conn)
+        else:
+            broadcast(message, client_list, conn)
 
+
+def broadcast(message, client_list, conn):
+    for client in client_list:
+        if client != conn:
+            client.send(message.encode())
+    print(message)
 
 
 def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT))
-    server.listen(CONNECTIONS)
-
+    server = up_server()
     client_list = []
 
     while True:
         conn, addr = server.accept()
-        username = conn.recv(1024).decode('utf-8')
+        print(f'Received connection from {conn.fileno()} {conn.getpeername()}')
+        username = conn.recv(1024).decode()
         client_list.append(conn)
 
-        join_msg = username + 'join the chat'
-        broadcast(join_msg, client_list)
+        join_message = "\n" + username + "join the chat."
+        broadcast(join_message, client_list, conn)
 
-        thread = threading.Thread(target=listen, args=(conn, client_list,))
+        thread = Thread(target=listen, args=(conn, client_list,))
         thread.daemon = True
         thread.start()
 
